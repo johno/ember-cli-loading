f9bafe20909635049c6284ad2e815aa8d57e0435
John Otander <johnotander@gmail.com>
2014-09-14 23:06:56 -0600

    Update addon name.

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 'use strict';
-
-// TODO: Change name and add any relevant imports.
-
 module.exports = {
-  name: 'ember-cli-addon-template',
+  name: 'ember-cli-loading',
 
   included: function included(app) {
     this.app = app;
     this._super.included(app);
-
-   // app.import("node_modules/some-dependency/path/to/file");
-   // app.import("bower_components/some-dependency/path/to/file");
   }
 };
